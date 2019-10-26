851733c182d41f4acd79494dadfdf3a24453bd36
lizhihong@cmcm.com <lizhihong@cmcm.com>
2019-10-26 21:50:51 +0800

    添加ar测试代码

--- arhome/src/main/java/com/example/arhome/ArHomeActivity.java
+++ arhome/src/main/java/com/example/arhome/ArHomeActivity.java
@@ -3,6 +3,10 @@ package com.example.arhome;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.alibaba.android.arouter.facade.annotation.Route;
+
+
+@Route(path = "/arhome/mainactivity", name="我的测试activity")
 public class ArHomeActivity extends AppCompatActivity {
 
     @Override
